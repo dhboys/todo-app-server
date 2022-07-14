3577a6ce2e905d0324d0da14a169bc448167ecf1
ekdldksp123 <ekdldksp123@naver.com>
2022-07-14 20:54:59 +0900

    :bug: fix export ConfigModule error

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { FeedModule } from './feed/modules/feed.module';
 import { DatabaseModule } from './database/mongo.module';
 import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CommonModule, DatabaseModule, FeedModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    CommonModule,
+    DatabaseModule,
+    FeedModule,
+  ],
 })
 export class AppModule {}

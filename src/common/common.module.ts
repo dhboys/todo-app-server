e6f181e0eff53a7d61992a1ff00efd5d06529ef9
ekdldksp123 <ekdldksp123@naver.com>
2022-07-19 20:36:47 +0900

    :hammer: combine graphql module with app.module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,8 @@
-import { GraphqlModule } from './graphql/graphql.module';
 import { Global, Module } from '@nestjs/common';
 import { ConfigShared } from './services/config.shared';
 
 //전역에서 공유되는 공통 서비스들을 묶어놓은 모듈
-const providers = [ConfigShared, GraphqlModule];
+const providers = [ConfigShared];
 @Global()
 @Module({
   providers,
